27837bd79e50859e16c17dd39594ae934c0e73e2
Markus Feng <zzz1112222595@hotmail.com>
2014-10-06 07:53:14 -0400

    Expanded ArrayHelper

--- src/com/github/assisstion/ModulePack/helper/ArrayHelper.java
+++ src/com/github/assisstion/ModulePack/helper/ArrayHelper.java
@@ -24,12 +24,16 @@ public final class ArrayHelper{
 	}
 
 	public static <T> T[] arrayCopy(T[] x){
+		return arrayCopy(x, true);
+	}
+
+	public static <T> T[] arrayCopy(T[] x, boolean notNull){
 		int z = 0;
 		T[] l = x.clone();
 		for(int i = 0; i < x.length; i++){
 			T y = x[i];
 			l[i] = null;
-			if(y != null){
+			if(!notNull || y != null){
 				l[z] = y;
 				z++;
 			}
@@ -39,14 +43,18 @@ public final class ArrayHelper{
 	}
 
 	public static int[] arrayCopy(int[] x){
+		return arrayCopy(x, false);
+	}
+
+	public static int[] arrayCopy(int[] x, boolean notZero){
 		int z = 0;
 		int[] l = new int[x.length + 1];
 		for(int i = 0; i < x.length; i++){
 			int y = x[i];
-			//if(y != 0){
-			l[z] = y;
-			z++;
-			//}
+			if(!notZero || y != 0){
+				l[z] = y;
+				z++;
+			}
 		}
 		int[] n = new int[z];
 		System.arraycopy(l, 0, n, 0, z);
@@ -54,14 +62,18 @@ public final class ArrayHelper{
 	}
 
 	public static byte[] arrayCopy(byte[] x){
+		return arrayCopy(x, false);
+	}
+
+	public static byte[] arrayCopy(byte[] x, boolean notZero){
 		int z = 0;
 		byte[] l = new byte[x.length + 1];
 		for(int i = 0; i < x.length; i++){
 			byte y = x[i];
-			//if(y != 0){
-			l[z] = y;
-			z++;
-			//}
+			if(!notZero || y != 0){
+				l[z] = y;
+				z++;
+			}
 		}
 		byte[] n = new byte[z];
 		System.arraycopy(l, 0, n, 0, z);
@@ -69,14 +81,18 @@ public final class ArrayHelper{
 	}
 
 	public static char[] arrayCopy(char[] x){
+		return arrayCopy(x, false);
+	}
+
+	public static char[] arrayCopy(char[] x, boolean notZero){
 		int z = 0;
 		char[] l = new char[x.length + 1];
 		for(int i = 0; i < x.length; i++){
 			char y = x[i];
-			//if(y != 0){
-			l[z] = y;
-			z++;
-			//}
+			if(!notZero || y != 0){
+				l[z] = y;
+				z++;
+			}
 		}
 		char[] n = new char[z];
 		System.arraycopy(l, 0, n, 0, z);
@@ -99,10 +115,28 @@ public final class ArrayHelper{
 	}
 
 	public static <T> void arrayPrint(T[] x){
-		T[] b = arrayCopy(x);
+		arrayPrint(x, "\n", true);
+	}
+
+	public static <T> void arrayPrint(T[] x, String separator){
+		arrayPrint(x, separator, false);
+	}
+
+	public static <T> void arrayPrint(T[] x, String separator, boolean finalSep){
+		T[] b = x;//arrayCopy(x);
+		boolean first = true;
 		for(int n = 0; n < b.length; n++){
+			if(first){
+				first = false;
+			}
+			else{
+				System.out.print(separator);
+			}
 			T w = b[n];
-			System.out.println(w);
+			System.out.print(w);
+		}
+		if(finalSep){
+			System.out.print(separator);
 		}
 	}
 
@@ -131,16 +165,11 @@ public final class ArrayHelper{
 	}
 
 	public static <T, S extends T> int positionIn(T[] x, S y){
-		for(int i = 0; i < x.length; i++){
-			if(x[i].equals(y)){
-				return i;
-			}
-		}
-		return -1;
+		return firstIndexOf(x, y);
 	}
 
 	public static void arrayPrint(int[] x) {
-		int[] b = arrayCopy(x);
+		int[] b = x;//arrayCopy(x);
 		for(int n = 0; n < b.length; n++){
 			int w = b[n];
 			System.out.println(w);
@@ -148,7 +177,7 @@ public final class ArrayHelper{
 	}
 
 	public static void arrayPrint(byte[] x) {
-		byte[] b = arrayCopy(x);
+		byte[] b = x;//arrayCopy(x);
 		for(int n = 0; n < b.length; n++){
 			byte w = b[n];
 			System.out.println(w);
@@ -156,7 +185,7 @@ public final class ArrayHelper{
 	}
 
 	public static void arrayPrint(char[] x) {
-		char[] b = arrayCopy(x);
+		char[] b = x;//arrayCopy(x);
 		for(int n = 0; n < b.length; n++){
 			char w = b[n];
 			System.out.println(w);
@@ -183,6 +212,36 @@ public final class ArrayHelper{
 		return integers;
 	}
 
+	public static <T, S extends T> int firstIndexOf(T[] x, S y){
+		return nthIndexOf(x, y, 0);
+	}
+
+	//Index n, not numeral n
+	public static <T, S extends T> int nthIndexOf(T[] x, S y, int n){
+		int counter = 0;
+		for(int i = 0; i < x.length; i++){
+			if(x[i].equals(y)){
+				if(counter >= n){
+					return i;
+				}
+				else{
+					counter++;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public static <T, S extends T> int lastIndexOf(T[] x, S y, int n){
+		int lastFound = -1;
+		for(int i = 0; i < x.length; i++){
+			if(x[i].equals(y)){
+				lastFound = i;
+			}
+		}
+		return lastFound;
+	}
+
 	public static <T, S extends T> Set<Pair<Integer, Integer>> indexOf(T[][] x, S y){
 		HashSet<Pair<Integer, Integer>> integers = new HashSet<Pair<Integer, Integer>>();
 		for(int i = 0; i < x.length; i++){
@@ -193,5 +252,38 @@ public final class ArrayHelper{
 			}
 		}
 		return integers;
+	}
+
+	public static <T, S extends T> Pair<Integer, Integer> firstIndexOf(T[][] x, S y){
+		return nthIndexOf(x, y, 0);
+	}
+
+	public static <T, S extends T> Pair<Integer, Integer> nthIndexOf(T[][] x, S y, int n){
+		int counter = 0;
+		for(int i = 0; i < x.length; i++){
+			for(int j = 0; j < x[i].length; j++){
+				if(x[i][j].equals(y)){
+					if(counter >= n){
+						return new Pair<Integer, Integer>(i, j);
+					}
+					else{
+						counter++;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public static <T, S extends T> Pair<Integer, Integer> lastIndexOf(T[][] x, S y){
+		Pair<Integer, Integer> lastFound = null;
+		for(int i = 0; i < x.length; i++){
+			for(int j = 0; j < x[i].length; j++){
+				if(x[i][j].equals(y)){
+					lastFound = new Pair<Integer, Integer>(i, j);
+				}
+			}
+		}
+		return lastFound;
 	}
 }
