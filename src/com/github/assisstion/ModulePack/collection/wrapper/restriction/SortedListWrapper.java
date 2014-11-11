c3a71e1de894cac3d1976292cbb4f0834ac06662
Markus Feng <zzz1112222595@hotmail.com>
2014-11-10 19:37:05 -0500

    Implemented SortedListWrapper

--- src/com/github/assisstion/ModulePack/collection/wrapper/restriction/SortedListWrapper.java
+++ src/com/github/assisstion/ModulePack/collection/wrapper/restriction/SortedListWrapper.java
@@ -1,12 +1,65 @@
 package com.github.assisstion.ModulePack.collection.wrapper.restriction;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
+import com.github.assisstion.ModulePack.collection.sort.InPlaceMergeSortHelper;
 import com.github.assisstion.ModulePack.collection.wrapper.ListWrapper;
 
 public class SortedListWrapper<T> extends ListWrapper<T>{
 
-	public SortedListWrapper(List<T> list){
+	protected Comparator<? super T> comp;
+
+	public SortedListWrapper(List<T> list, Comparator<? super T> comp){
 		super(list);
+		this.comp = comp;
+		InPlaceMergeSortHelper.sort(list, comp);
+	}
+
+	@Override
+	public boolean add(T t){
+		int i = 0;
+		for(T item : get()){
+			if(comp.compare(t, item) > 0){
+				break;
+			}
+			i++;
+		}
+		super.add(i, t);
+		return true;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> coll){
+		boolean changed = false;
+		for(T t : coll){
+			if(add(t)){
+				changed = true;
+			}
+		}
+		return changed;
+	}
+
+	//Ignores index
+	@Override
+	public boolean addAll(int index, Collection<? extends T> coll){
+		return addAll(coll);
+		//throw new UnsupportedOperationException();
+	}
+
+	//Ignores index
+	@Override
+	public T set(int index, T element){
+		add(element);
+		return null;
+		//throw new UnsupportedOperationException();
+	}
+
+	//Ignores index
+	@Override
+	public void add(int index, T element){
+		add(element);
+		//throw new UnsupportedOperationException();
 	}
 }
